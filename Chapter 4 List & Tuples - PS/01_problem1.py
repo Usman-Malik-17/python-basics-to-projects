17075f45147202b563c9c11cbea8e1cc3d2f837f
Usman-Malik-17 <musmanmalik28.21@gmail.com>
2026-02-08 10:49:30 +0500

    Store fruits name in a list entered by user

---
+++ Chapter 4 List & Tuples - PS/01_problem1.py
@@ -0,0 +1,9 @@
+fruits = []
+fruits.append(input("Enter first fruit:"))
+fruits.append(input("Enter Second fruit:"))
+fruits.append(input("Enter Third fruit:"))
+fruits.append(input("Enter Fourth fruit:"))
+fruits.append(input("Enter Fifth fruit:"))
+fruits.append(input("Enter Sixth fruit:"))
+fruits.append(input("Enter Seventh fruit:"))
+print(fruits)
