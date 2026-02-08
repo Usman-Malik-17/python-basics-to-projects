015838b510b37fc8d58a1f6e1bc486ae8f7b8fdc
Usman-Malik-17 <musmanmalik28.21@gmail.com>
2026-02-08 11:00:19 +0500

    Accept marks of Students and then sort it

---
+++ Chapter 4 List & Tuples - PS/02_problem2.py
@@ -0,0 +1,12 @@
+marks = []
+marks.append(int(input("Enter first student marks:")))
+marks.append(int(input("Enter Second student marks:")))
+marks.append(int(input("Enter Third student marks:")))
+marks.append(int(input("Enter Fourth student marks:")))
+marks.append(int(input("Enter Fifth student marks:")))
+marks.append(int(input("Enter Sixth student marks:")))
+marks.append(int(input("Enter Seventh student marks:")))
+print(marks)
+
+marks.sort()
+print(marks)
